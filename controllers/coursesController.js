a3f87e53f02f3d2497942c60d02eec409ac2f398
paszo <paszo2008@gmail.com>
2020-07-30 22:05:27 +0200

    Added ajax courses modal

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -1,4 +1,6 @@
 const Course = require('../models/course');
+const httpStatus = require('http-status-codes');
+const User = require('../models/user');
 
 const getCourseParams = (body) => {
     return {
@@ -22,11 +24,7 @@ module.exports = {
             });
     },
     indexView: (req, res) => {
-        if (req.query.format === 'json') {
-            res.json(res.locals.courses);
-        } else {
-            res.render("courses/index");
-        }
+        res.render("courses/index");
     },
     new: (req, res) => {
         res.render("courses/new");
@@ -104,5 +102,63 @@ module.exports = {
                 console.log(`Error deleting course by ID: ${error.message}`);
                 next();
             });
+    },
+    respondJSON: (req, res) => {
+        res.json({
+            status: httpStatus.OK,
+            data: res.locals
+        });
+    },
+    errorJSON: (error, req, res, next) => {
+        let errorObject;
+
+        if(error) {
+            errorObject = {
+                status: httpStatus.INTERNAL_SERVER_ERROR,
+                message: error.message
+            };
+        } else {
+            errorObject = {
+                status: httpStatus.INTERNAL_SERVER_ERROR,
+                message: "Unknown Error."
+            };
+        }
+        res.json(errorObject);
+    },
+    join: (req, res, next) => {
+        let courseId = req.params.id;
+        let currentUser = req.user;
+
+        if (currentUser) {
+            User.findByIdAndUpdate(currentUser, {
+                $addToSet: {
+                    courses: courseId
+                }
+            })
+                .then(() => {
+                    res.locals.success = true;
+                    next();
+                })
+                .catch(error => {
+                    next(error);
+                });
+        } else {
+            next(new Error("User must log in."));
+        }
+    },
+    filterUserCourses: (req, res, next) => {
+        let currentUser = res.locals.currentUser;
+        if (currentUser) {
+            let mappedCourses = res.locals.courses.map((course) => {
+                let userJoined = currentUser.courses.some((userCourse) => {
+                    return userCourse.equals(course._id);
+                });
+                return Object.assign(course.toObject(), {joined: userJoined});
+            });
+            res.locals.courses = mappedCourses;
+            next();
+        } else {
+            next();
+        }
     }
 };
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ const subscriberRoutes = require('./subscriberRoutes');
 const courseRoutes = require('./courseRoutes');
 const homeRoutes = require('./homeRoutes');
 const errorRoutes = require('./errorRoutes');
+const apiRoutes = require('./apiRoutes');
 
 router.use('/users', userRoutes);
 router.use('/subscribers', subscriberRoutes);
 router.use('/courses', courseRoutes);
+router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/', errorRoutes);
+
 
 module.exports = router;
